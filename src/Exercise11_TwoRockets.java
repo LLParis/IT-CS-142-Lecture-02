bf1d2278df05edd8ffee4f0f26401d2fe1e070fd
Your Name <you@example.com>
2024-01-16 12:52:47 -0800

    Initial Commit

---
+++ src/Exercise11_TwoRockets.java
@@ -0,0 +1,3 @@
+// public class ???WHAT SHOULD YOU PUT HERE??? {
+
+// }
---
+++ src/Exercise8_Stewie2.java
@@ -0,0 +1,23 @@
+ public class Exercise8_Stewie2 {
+    public static void main(String[] args) {
+        int x = 5;
+
+        for (int i = 0; i < 5; i++) {
+            divisor();
+            vim();
+        }
+        divisor();
+    }
+
+     public static void top() {
+         System.out.println("//////////////////////");
+     }
+     public static void divisor() {
+         System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+     }
+
+     public static void vim() {
+        System.out.println("|| Victory is mine! ||");
+     }
+
+ }
